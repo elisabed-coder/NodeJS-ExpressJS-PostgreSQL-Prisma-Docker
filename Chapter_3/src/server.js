37b251b4de6b0e26ab76353164bc0f79d6931220
Elisabed <134492535+elisabed-coder@users.noreply.github.com>
2025-03-10 22:08:47 +0400

    update server js

---
+++ Chapter_3/src/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+const PORT = process.env.PORT || 5003;
+
+//GET the file path from the url of the current module
+const __filename = fileURLToPath(import.meta.url);
+
+//GEt the directory name from the file path
+const __dirname = dirname(__filename);
+
+//MIIDLEWARE
+app.use(express.json());
+
+//serves the HTML file from from the /public directory
+//Tells express to serve all files form the public folder as static assets
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server has started:${PORT}`);
+});
