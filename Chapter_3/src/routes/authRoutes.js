25409a9b61acaa22dd8ac68e8358cc6db522b645
Elisabed <134492535+elisabed-coder@users.noreply.github.com>
2025-03-11 16:19:39 +0400

    create register method in authRoutes

--- Chapter_3/src/routes/authRoutes.js
+++ Chapter_3/src/routes/authRoutes.js
@@ -7,7 +7,40 @@ const router = express.Router();
 
 router.post("/register", (req, res) => {
   const { username, password } = req.body;
-  console.log(username, password);
+
+  // encrypt the password
+  const hashedPassword = bcrypt.hashSync(password, 8);
+
+  // save the new user and hashed password to the db
+  try {
+    // prepare method -  prepares a SQL query
+    const inserUser = db.prepare(
+      `INSERT INTO users (username, password) VALUES(?,?)`
+    );
+    // Run SQL command
+    const result = inserUser.run(username, hashedPassword);
+
+    // now that we have a user, I want to add their first todo for them.
+    const defaultTodo = "add your first todo";
+    const insertTodo = db.prepare(
+      `INSERT INTO tod os(user_id, task) VALUES(?,?)`
+    );
+    insertTodo.run(result.lastInsertRowid, defaultTodo);
+
+    // create a token
+
+    const token = jwt.sign(
+      { id: result.lastInsertRowid },
+      process.env.JWT_SECRET,
+      { expiresIn: "24hr" }
+    );
+
+    //send JSON to user
+    res.json({ token });
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(503);
+  }
   res.sendStatus(201);
 });
 
